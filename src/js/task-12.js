b8637ac1e4d2bdaf4f85302108ef45339f0fe54b
IvaZach <ivazacherniuk@gmail.com>
2023-10-17 13:45:14 +0300

    установочные файлы

--- src/js/task-12.js
+++ src/js/task-12.js
@@ -1,35 +1,35 @@
-import '../css/common.css';
+// import '../css/styles_11.css';
 // import BSN from "bootstrap.native";
 
-const refs = {
-  modal: document.querySelector('#subscription-modal'),
-  subscribeBtn: document.querySelector('button[data-subscribe]'),
-};
-const PROMPT_DELAY = 3000;
-const MAX_PROMPT_ATTEMPTS = 3;
-let promptCounter = 0;
-let hasSubscribed = false;
-const modal = new BSN.Modal('#subscription-modal');
+// const refs = {
+//   modal: document.querySelector('#subscription-modal'),
+//   subscribeBtn: document.querySelector('button[data-subscribe]'),
+// };
+// const PROMPT_DELAY = 3000;
+// const MAX_PROMPT_ATTEMPTS = 3;
+// let promptCounter = 0;
+// let hasSubscribed = false;
+// const modal = new BSN.Modal('#subscription-modal');
 
-openModal();
+// openModal();
 
-refs.modal.addEventListener('hide.bs.modal', openModal);
-refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
+// refs.modal.addEventListener('hide.bs.modal', openModal);
+// refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
 
-function openModal() {
-  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
-    console.log('Максимальное кол-во надоеданий или подписался');
-    return;
-  }
+// function openModal() {
+//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+//     console.log('Максимальное кол-во надоеданий или подписался');
+//     return;
+//   }
 
-  setTimeout(() => {
-    console.log('Открываем надоедалку');
-    modal.show();
-    promptCounter += 1;
-  }, PROMPT_DELAY);
-}
+//   setTimeout(() => {
+//     console.log('Открываем надоедалку');
+//     modal.show();
+//     promptCounter += 1;
+//   }, PROMPT_DELAY);
+// }
 
-function onSubscribeBtnClick() {
-  hasSubscribed = true;
-  modal.hide();
-}
+// function onSubscribeBtnClick() {
+//   hasSubscribed = true;
+//   modal.hide();
+// }
